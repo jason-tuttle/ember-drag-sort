bae6bc6c3ec3110139207d8b25d2f7abd4e1f87d
pqmcgill <pqmcgill@gmail.com>
2019-11-15 14:49:32 -0500

    opt-in api for access wrapper element

--- tests/integration/components/drag-sort-list-test.js
+++ tests/integration/components/drag-sort-list-test.js
@@ -214,3 +214,64 @@ test('nested drag handle', withChai(async function (expect) {
     targetIndex : 1
   })
 }))
+
+test('customize internal dragSortItem wrapper', withChai(async function (expect) {
+  const items = A([
+    {name : 'foo'},
+    {name : 'bar'},
+    {name : 'baz'},
+  ])
+
+  const dragEndCallback = sinon.spy()
+
+  this.setProperties({items, dragEndCallback})
+
+  this.render(hbs`
+    {{#drag-sort-list
+      items         = items
+      dragEndAction = (action dragEndCallback)
+      yieldDragSortItemComponent = true
+      as |item|
+    }}
+      {{#item.Component class="foo"}}
+        <div class="handle">
+          <div class="handle2">handle</div>
+        </div>
+        <div>
+          {{item.name}}
+        </div>
+      {{/item.Component}}
+    {{/drag-sort-list}}
+  `)
+
+  // check for custom class in addition to internal class. Test that drag and drop sorting still works
+  const $item0 = this.$('.dragSortItem.foo:eq(0)')
+  const $item1 = this.$('.dragSortItem.foo:eq(1)')
+
+  // const itemOffset = $item0.offset()
+
+  trigger($item0, 'dragstart')
+  trigger($item1, 'dragover', false)
+  trigger($item0, 'dragend')
+
+  await wait()
+
+  expect(dragEndCallback).not.called
+
+  trigger($item0.find('.handle2'), 'dragstart')
+  trigger($item1, 'dragover', false)
+  trigger($item0, 'dragend')
+
+  await wait()
+
+  expect(dragEndCallback).calledOnce
+
+  expect(dragEndCallback).calledWithExactly({
+    group       : undefined,
+    draggedItem : items.objectAt(0),
+    sourceList  : items,
+    targetList  : items,
+    sourceIndex : 0,
+    targetIndex : 1
+  })
+}))
